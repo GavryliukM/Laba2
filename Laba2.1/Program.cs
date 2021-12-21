83415cee94d0efe5925628e037e10549d75a992a
myhailohavryliuk <myhailohavryliuk@MYHAILOHAVRCEA9>
2021-12-21 16:10:22 +0200

    Add project files.

---
+++ Laba2.1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace LB2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animals = new Animal("", "", " ", 0);
+
+            for (int j = 0; j < animals.arr.Length; j++)
+            {
+                Console.Write("Введiть назву: ");
+                string name = Console.ReadLine();
+                Console.Write("Введiть групу: ");
+                string group = Console.ReadLine();
+                Console.Write("Введiть житло: ");
+                string country = Console.ReadLine();
+                Console.Write("Введiть чисельність: ");
+                int population = Convert.ToInt32(Console.ReadLine());
+                animals.arr[j] = new Animal(name, group, country, population);
+            }
+
+            Animal.count--;
+
+            void MinPop()
+            {
+                int y = int.MaxValue;
+                int r = 0;
+                for (int i = 0; i < animals.arr.Length; i++)
+                {
+
+                    if (animals.arr[i].population < y)
+                    {
+                        y = animals.arr[i].population;
+                        r = i;
+                    }
+
+
+                }
+                Console.WriteLine("Наймолодша популяція " + animals.arr[r].name);
+            }
+
+            Console.WriteLine("Об'єкти: " + (Animal.count) + "\n");
+            MinPop();
+            if (Animal.count < 10)
+                Console.WriteLine("Об'єктiв менше граничного значення");
+            else if (Animal.count == 10)
+                Console.WriteLine("Об'єктiв рiвно граничне значення");
+            else if (Animal.count > 10)
+                Console.WriteLine("Об'єктiв бiльше граничного значення");
+        }
+    }
+
+    class Animal
+    {
+        public Animal[] arr = new Animal[9];
+        public static int count = 0;
+        public string group;
+        public string name;
+        public string country;
+        public int population;
+        public Animal(string name, string group, string country, int population)
+        {
+            this.group = group;
+            this.name = name;
+            this.country = country;
+            this.population = population;
+            count++;
+        }
+    }
+}
---
+++ Laba2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LB2
+{
+    public class LB2
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int max = arr[0];
+
+            Console.WriteLine("Enter values:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("arr[{0}] = ", i);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Array:");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+            for (int i = 1; i < arr.Length; i++)
+                if (arr[i] > max)
+                    max = arr[i];
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] < 0)
+                    arr[i] = max;
+            Console.WriteLine("Result:");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
